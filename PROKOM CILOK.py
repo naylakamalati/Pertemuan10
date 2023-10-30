71f71c7530277bb7c1d4ccb95db83476b28fc5d3
naylakamalati <naylakamalatiputri@mail.ugm.ac.id>
2023-10-30 17:56:59 +0700

    Tes

---
+++ PROKOM CILOK.py
@@ -0,0 +1,7 @@
+x = str(input("Tebak makanan kesukaan saya :"))
+if x == "Cilok":
+    print("Ya, Anda BENAR")
+elif x == "cilok":
+    print("Iya, tapi nulisnya yang bener dong!")
+else:
+    print("Gak ada yang lebih enak dadri {x} ni??")
---
+++ food.py
@@ -0,0 +1,8 @@
+food = str(input("Tebak makanan kesukaan saya: "))
+
+if food == "Cilok":
+    print("Ya, Anda BENAR")
+elif food == "cilok":
+    print("Iya, tapi nulisnya yang bener dong!")
+else:
+    print(f"Gak ada yang lebih enak dari {food} nih??")
